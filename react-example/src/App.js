27fd24f425e8ddf95f9020436680c3c30105d5d3
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-03-27 19:47:46 +0000

    Add use state js files

--- react-example/src/App.js
+++ react-example/src/App.js
@@ -2,12 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 import ControlledForm from './components/ControlledForm';
 import ControlledFormHooks from './components/ControlledFormHooks';
+import UseStateWithArrays from './components/UseStateWithArrays';
+import UseStateWithObjects from './components/UseStateWithObjects';
 
 function App() {
   return (
     <div className="App">
       {/* <ControlledForm /> */}
-      <ControlledFormHooks />
+      {/* <ControlledFormHooks /> */}
+      {/* <UseStateWithArrays /> */}
+      <UseStateWithObjects />
     </div>
   );
 }
