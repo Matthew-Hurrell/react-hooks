73b93c6e7f0d5d734b0a0fcd1849f5481dda6057
Matthew Hobbs-Hurrell <matthewhobbshurrell@icloud.com>
2023-03-28 19:44:43 +0000

    Add use effect counter file and logic

--- react-example/src/components/UseEffectCounter.js
+++ react-example/src/components/UseEffectCounter.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react'
 
 function UseEffectCounter() {
     const [count, setCount] = useState(0);
+    const [time, setTime] = useState(0);
     useEffect(() => {
         document.title = count;
     }, [count]);
@@ -9,6 +10,11 @@ function UseEffectCounter() {
     useEffect(() => {
         document.title = count2;
     }, [count2]);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setTime(time => time + 1)
+        }, 1000);
+    }, []);
   return (
     <div>
         <button onClick={
@@ -21,6 +27,7 @@ function UseEffectCounter() {
         }>
             Increment Count 2 ({count2})
         </button>
+        <h2>Time is {time}</h2>
     </div>
   )
 }
